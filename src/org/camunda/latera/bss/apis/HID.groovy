b2030d97ac8dc75bc56272eb0384966a602cdc04
dolfinus <m.martynov@latera.ru>
2018-12-27 17:02:56 +0300

    Add class for HID API

---
+++ src/org/camunda/latera/bss/apis/HID.groovy
@@ -0,0 +1,64 @@
+package org.camunda.latera.bss.apis
+
+import groovy.net.xmlrpc.*
+import org.camunda.latera.bss.utils.StringUtil
+import org.camunda.latera.bss.utils.Oracle
+import org.camunda.latera.bss.apis.hid.Table
+import org.camunda.bpm.engine.delegate.DelegateExecution
+
+//@Mixin(Table)
+class HID implements Table {
+  XMLRPCServerProxy proxy
+
+  HID(DelegateExecution execution) {
+    def url      = execution.getVariable('hidUrl')  ?: 'http://hid:10080'
+    def user     = execution.getVariable('hidUser') ?: 'hydra'
+    def password = execution.getVariable('hidPassword')
+
+    this.proxy = new XMLRPCServerProxy(url)
+    this.proxy.setBasicAuth(user, password)
+  }
+
+  Object queryDatabase(String query, Boolean asMap = true) {
+    List result = []
+    List rows = this.proxy.invokeMethod('SELECT', [query]).SelectResult
+    if (rows) {
+      rows.each{ row ->
+        // There is row number, just remove it
+        row.removeAt(0)
+
+        // Convert codepage from
+        List convertedRow = []
+        row.each{ value ->
+          if (StringUtil.isString(value)) {
+            convertedRow.add(StringUtil.varcharToUnicode(value))
+          } else {
+            convertedRow.add(value)
+          }
+        }
+
+        if (asMap) {
+          // Use "'VC_VALUE', VC_VALUE" format because SELECT procedure don't return column names
+          LinkedHashMap mappedRow = (convertedRow as Object[]).toSpreadMap()
+          result.add(mappedRow)
+        } else {
+          result.add(convertedRow)
+        }
+      }
+    }
+  
+    return result
+  }
+
+  LinkedHashMap queryFirst(String query, Boolean asMap = true) {
+    this.queryDatabase(query, asMap)?.getAt(0)
+  }
+
+  Object execute(String execName ,LinkedHashMap params) {
+    LinkedHashMap encodedParams = [:]
+    params.each{ key, value ->
+      encodedParams[key] = Oracle.encodeNull(value)
+    }
+    return this.proxy.invokeMethod(execName, [encodedParams])
+  }
+}
---
+++ src/org/camunda/latera/bss/apis/hid/Table.groovy
@@ -0,0 +1,155 @@
+package org.camunda.latera.bss.apis.hid
+
+trait Table {
+  static LinkedHashMap tableColumnsCache = [:]
+  static List defaultFields = null
+  static LinkedHashMap defaultWhere = [:]
+  static List defaultOrder = null
+
+  List getTableColumns(String tableName, String tableOwner = 'AIS_NET') {
+    String tableFullName = "${tableOwner}.${tableName}"
+    if (this.tableColumnsCache.containsKey(tableFullName)) {
+      return this.tableColumnsCache[tableFullName]
+    } else {
+      List columnsList = null
+
+      List result = this.queryDatabase("""
+        SELECT COLUMN_NAME
+        FROM   ALL_TAB_COLUMNS
+        WHERE  TABLE_NAME = '${tableName}'
+        AND    OWNER      = '${tableOwner}'
+      """, false)
+
+      columnsList = result*.getAt(0) //get only first column values
+
+      if (columnsList) {
+        this.tableColumnsCache[tableFullName] = columnsList
+      }
+      return columnsList
+    }
+  }
+
+  List getTableData(
+    String tableName,
+    fields = this.defaultFields,
+    LinkedHashMap where = this.defaultWhere,
+    List order = this.defaultOrder
+  ) {
+    String query = "SELECT "
+    
+    if (fields == '*' || fields == null) {
+      fields = this.getTableColumns(tableName)
+    }
+    
+    fields.each{ field ->
+      query += """
+      '${field.toLowerCase()}', ${field}
+      """ + (field == fields.last() ? '' : ',')
+    }
+    query += """
+    FROM ${tableName}
+    """
+
+    if (where?.size() > 0) {
+      query += """
+      WHERE 1 = 1
+      """
+
+      where.each{ field, value ->
+        if (value instanceof LinkedHashMap) {
+          value.each { condition, content ->
+            query += """
+              AND ${field} ${condition} ${content}
+            """
+          }
+        } else if (value instanceof List) {
+          value.each { condition ->
+            query += """
+              AND ${field} ${condition}
+            """
+          }
+        } else if (value instanceof String) {
+          query += """
+            AND ${field} = '${value}'
+          """
+        } else {
+          query += """
+            AND ${field} = ${value}
+          """
+        }
+      }
+    }
+
+    if (order?.size() > 0) {
+      query += """
+      ORDER BY
+      """
+      order.each{column ->
+        query += """
+        ${column}
+        """ + (column == order.last() ? '' : ',')
+      }
+    }
+    return this.queryDatabase(query)
+  }
+
+  List getTableData(
+    LinkedHashMap options,
+    String tableName
+  ) {
+    LinkedHashMap params = [
+      fields: this.defaultFields,
+      where:  this.defaultWhere,
+      order:  this.defaultOrder
+    ] + options
+    return this.getTableData(tableName, params.fields, params.where, params.order)
+  }
+
+  List getTableData(
+    LinkedHashMap input
+  ) {
+    LinkedHashMap params = [
+      tableName: '',
+      fields:    this.defaultFields,
+      where:     this.defaultWhere,
+      order:     this.defaultOrder
+    ] + input
+    return this.getTableData(params.tableName, params.fields, params.where, params.order)
+  }
+
+  Object getTableFirst(
+    String tableName,
+    fields = this.defaultFields,
+    LinkedHashMap where = this.defaultWhere,
+    List order = this.defaultOrder
+  ) {
+    if (fields instanceof String && fields != '*') {
+      return this.getTableData(tableName, [fields], where)?.getAt(0)?."${fields}"
+    }
+    return this.getTableData(tableName, fields, where)?.getAt(0)
+  }
+
+  Object getTableFirst(
+    LinkedHashMap options,
+    String tableName
+  ) {
+    LinkedHashMap params = [
+      fields: this.defaultFields,
+      where:  this.defaultWhere,
+      order:  this.defaultOrder
+    ] + options
+    return this.getTableFirst(tableName, params.fields, params.where, params.order)
+  }
+
+  Object getTableFirst(
+    LinkedHashMap input
+  ) {
+    LinkedHashMap params = [
+      tableName: '',
+      fields:    this.defaultFields,
+      where:     this.defaultWhere,
+      order:     this.defaultOrder
+    ] + input
+    return this.getTableFirst(params.tableName, params.fields, params.where, params.order)
+  }
+}
