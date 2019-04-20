5f5f6b2ada4f36f62d8c338976826d84b7dccf33
dolfinus <martinov_m_s_@mail.ru>
2019-04-20 16:53:54 +0300

    Fix contract autonumbering while creating new contract w/o number

--- src/org/camunda/latera/bss/connectors/hid/hydra/Contract.groovy
+++ src/org/camunda/latera/bss/connectors/hid/hydra/Contract.groovy
@@ -5,9 +5,10 @@ import org.camunda.latera.bss.utils.Oracle
 import java.time.LocalDateTime
 
 trait Contract {
-  private static String CONTRACTS_TABLE = 'SD_V_CONTRACTS'
-  private static String CONTRACT_TYPE   = 'DOC_TYPE_SubscriberContract'
-  private static String DEFAULT_CONTRACT_WORKFLOW = 'WFLOW_SubscriberContract'
+  private static String  CONTRACTS_TABLE = 'SD_V_CONTRACTS'
+  private static String  CONTRACT_TYPE   = 'DOC_TYPE_SubscriberContract'
+  private static String  DEFAULT_CONTRACT_WORKFLOW    = 'WFLOW_SubscriberContract'
+  private static Integer DEFAULT_CONTRACT_WORKFLOW_ID = 10021
 
  def getContractsTable() {
     return CONTRACTS_TABLE
@@ -26,7 +27,7 @@ trait Contract {
   }
 
   def getDefaultContractWorkflowId() {
-    return getRefIdByCode(DEFAULT_CONTRACT_WORKFLOW)
+    return DEFAULT_CONTRACT_WORKFLOW_ID
   }
 
   LinkedHashMap getContract(def docId) {
@@ -59,10 +60,9 @@ trait Contract {
   }
 
   LinkedHashMap putContract(LinkedHashMap input) {
-    LinkedHashMap params = mergeParams([
+    def defaultParams = [
       docId       : null,
       parentDocId : null,
-      reasonDocId : null,
       workflowId  : getDefaultContractWorkflowId(),
       providerId  : getFirmId(),
       receiverId  : null,
@@ -70,34 +70,47 @@ trait Contract {
       beginDate   : DateTimeUtil.now(),
       endDate     : null,
       number      : null
-    ], input)
+    ]
+    LinkedHashMap params = mergeParams(defaultParams, input)
     try {
-      logger.info("Putting contract with number ${params.number} parent ${params.parentDocId}, begin date ${params.beginDate} and end date ${params.endDate}")
-      LinkedHashMap contract = hid.execute('SD_CONTRACTS_PKG.SD_CONTRACTS_PUT', [
-        num_N_DOC_ID        : params.docId,
-        num_N_DOC_TYPE_ID   : getContractTypeId(),
-        num_N_PARENT_DOC_ID : params.parentDocId,
-        num_N_REASON_DOC_ID : params.reasonDocId,
-        num_N_WORKFLOW_ID   : params.workflowId,
-        dt_D_BEGIN          : params.beginDate,
-        dt_D_END            : params.endDate,
-        vch_VC_DOC_NO       : params.number
-      ])
-      logger.info("   Contract ${contract.num_N_DOC_ID} was put successfully!")
+      def paramsNames = (defaultParams.keySet() as List) - ['parentDocId', 'providerId', 'receiverId']
+      LinkedHashMap contract = [:]
+      if (params.subMap(paramsNames) == defaultParams.subMap(paramsNames)) {
+        logger.info("Creating new contract with receiver ${params.receiverId} and parent ${params.parentDocId}")
+        contract = hid.execute('SI_USERS_PKG.CREATE_CONTRACT', [
+          num_N_USER_ID          : params.receiverId,
+          num_N_FIRM_ID          : params.providerId,
+          num_N_BASE_CONTRACT_ID : params.parentDocId
+        ])
+        contract.num_N_DOC_ID = contract.num_N_CONTRACT_ID
+        logger.info("   Contract ${contract.num_N_DOC_ID} was put successfully!")
+      } else {
+        logger.info("Putting contract with number ${params.number} parent ${params.parentDocId}, begin date ${params.beginDate} and end date ${params.endDate}")
+        contract = hid.execute('SD_CONTRACTS_PKG.SD_CONTRACTS_PUT', [
+          num_N_DOC_ID        : params.docId,
+          num_N_DOC_TYPE_ID   : getContractTypeId(),
+          num_N_PARENT_DOC_ID : params.parentDocId,
+          num_N_WORKFLOW_ID   : params.workflowId,
+          dt_D_BEGIN          : params.beginDate,
+          dt_D_END            : params.endDate,
+          vch_VC_DOC_NO       : params.number
+        ])
+        logger.info("   Contract ${contract.num_N_DOC_ID} was put successfully!")
 
-      putDocumentSubject([
-        docId      : contract.num_N_DOC_ID,
-        subjectId  : params.providerId,
-        roleId     : getProviderRoleId(),
-        workflowId : params.workflowId
-      ])
-      putDocumentSubject([
-        docId      : contract.num_N_DOC_ID,
-        subjectId  : params.receiverId,
-        roleId     : getReceiverRoleId(),
-        workflowId : params.workflowId
-      ])
-      actualizeDocument(contract.num_N_DOC_ID)
+        putDocumentSubject([
+          docId      : contract.num_N_DOC_ID,
+          subjectId  : params.providerId,
+          roleId     : getProviderRoleId(),
+          workflowId : params.workflowId
+        ])
+        putDocumentSubject([
+          docId      : contract.num_N_DOC_ID,
+          subjectId  : params.receiverId,
+          roleId     : getReceiverRoleId(),
+          workflowId : params.workflowId
+        ])
+        actualizeDocument(contract.num_N_DOC_ID)
+      }
       return contract
     } catch (Exception e){
       logger.error("   Error while putting contract!")
