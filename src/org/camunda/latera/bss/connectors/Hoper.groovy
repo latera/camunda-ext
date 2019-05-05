e5493e9a684e435287d4a10bd3ff66cdadc86a94
dolfinus <martinov_m_s_@mail.ru>
2019-05-06 01:00:55 +0300

    Add support of v1 to Hoper class

--- src/org/camunda/latera/bss/connectors/Hoper.groovy
+++ src/org/camunda/latera/bss/connectors/Hoper.groovy
@@ -2,16 +2,17 @@ package org.camunda.latera.bss.connectors
 
 import org.camunda.latera.bss.http.HTTPRestProcessor
 import org.camunda.latera.bss.logging.SimpleLogger
+import org.camunda.latera.bss.utils.Base64Converter
 import org.camunda.bpm.engine.delegate.DelegateExecution
 
 class Hoper {
-  HTTPRestProcessor processor
-  LinkedHashMap headers
+  String url
+  private String user
+  private String password
+  String version
+  HTTPRestProcessor http
   DelegateExecution execution
   SimpleLogger logger
-  String url
-  String version
-  LinkedHashMap auth
 
   Hoper(DelegateExecution execution) {
     this.execution = execution
@@ -19,24 +20,47 @@ class Hoper {
 
     this.url      = execution.getVariable("hoperUrl")
     this.version  = execution.getVariable("hoperVersion") ?: '2'
-    this.auth     = [
-      login:    execution.getVariable("hydraUser"),
-      password: execution.getVariable("hydraPassword")
+    this.user     = execution.getVariable("hydraUser")
+    this.password = execution.getVariable("hydraPassword")
+    this.http     = new HTTPRestProcessor(baseUrl   : this.url,
+                                          execution : execution)
+  }
+
+  private def authToken() {
+    def auth = [
+      session: [
+        login    : user,
+        password : password
+      ]
     ]
-    this.processor = new HTTPRestProcessor(baseUrl   : this.url,
-                                           execution : execution)
+    return http.sendRequest('get',
+                            path : "/rest/v${this.version}/login",
+                            body : auth,
+                            supressRequestBodyLog  : true,
+                            supressResponseBodyLog : true)?.session?.token
+  }
+
+  private def authBasic() {
+    def auth = "${user}:${password}"
+    return Base64Converter.to(auth)
+  }
+
+  private def authHeader() {
+    if (version.toString() == '1') {
+      return ['Authorization': "Basic ${authBasic}"]
+    }
+    if (version.toString() == '2') {
+      return ['Authorization': "Token token=\"${authToken}\""]
+    }
+    return []
   }
 
   def sendRequest(LinkedHashMap input, String method = 'get') {
-    def token   = this.processor.sendRequest(path: "/rest/v${this.version}/login", body: [session: this.auth], supressRequestBodyLog: true, supressResponseBodyLog: true, 'get')?.session?.token
-    def headers = [
-      'Authorization': "Token token=\"${token}\""
-    ]
     if (!input.headers) {
       input.headers = [:]
     }
-    input.headers += headers
-    input.path = "/rest/v${this.version}/${input.path}".toString()
-    return this.processor.sendRequest(input, method)
+    input.headers += authHeader
+    input.path = "/rest/v${version}/${input.path}".toString()
+    return http.sendRequest(input, method)
   }
 }
