69293ae0bf06ff50396f9f6444b0b80cea4a8253
dolfinus <m.martynov@latera.ru>
2019-05-17 19:08:33 +0300

    Add get refs methods into hid.Hydra

--- src/org/camunda/latera/bss/connectors/hid/hydra/Ref.groovy
+++ src/org/camunda/latera/bss/connectors/hid/hydra/Ref.groovy
@@ -12,42 +12,167 @@ trait Ref {
     return REFS_TABLE
   }
 
+  LinkedHashMap getRef(def refId) {
+    LinkedHashMap where = [
+      n_ref_id: refId
+    ]
+    return hid.getTableFirst(getRefsTable(), where: where)
+  }
+
+  List getRefsBy(LinkedHashMap input) {
+    LinkedHashMap params = mergeParams([
+      refId      : null,
+      refTypeId  : null,
+      parRefId   : null,
+      baseRefId  : null,
+      name       : null,
+      code       : null,
+      string     : null,
+      stirng2    : null,
+      string3    : null,
+      bool       : null,
+      bool2      : null,
+      bool3      : null,
+      date       : null,
+      date2      : null,
+      date3      : null,
+      number     : null,
+      number2    : null,
+      number3    : null,
+      isEditable : null,
+      isManual   : null
+    ], input)
+    LinkedHashMap where = [:]
+
+    if (params.refId) {
+      where.n_ref_id = params.refId
+    }
+    if (params.refTypeId) {
+      where.n_ref_type_id = params.refTypeId
+    }
+    if (params.parRefId) {
+      where.n_par_ref_id = params.parRefId
+    }
+    if (params.baseRefId) {
+      where.n_ref_base_id = params.baseRefId
+    }
+    if (params.name) {
+      where.vc_name = params.name
+    }
+    if (params.code) {
+      where.vc_code = params.code
+    }
+    if (params.string) {
+      where.vc_value = params.string
+    }
+    if (params.string2) {
+      where.vc_value2 = params.string2
+    }
+    if (params.string3) {
+      where.vc_value3 = params.string3
+    }
+    if (params.bool != null) {
+      where.c_value = Oracle.encodeBool(params.bool)
+    }
+    if (params.bool2 != null) {
+      where.c_value2 = Oracle.encodeBool(params.bool2)
+    }
+    if (params.bool3 != null) {
+      where.c_value3 = Oracle.encodeBool(params.bool3)
+    }
+    if (params.date) {
+      where.d_value = date
+    }
+    if (params.date2) {
+      where.d_value2 = date2
+    }
+    if (params.date3) {
+      where.d_value = date3
+    }
+    if (params.number) {
+      where.n_value = number
+    }
+    if (params.number2) {
+      where.n_value = number2
+    }
+    if (params.number3) {
+      where.n_value = number3
+    }
+    if (params.isEditable != null) {
+      where.c_fl_editable = Oracle.encodeBool(params.isEditable)
+    }
+    if (params.isManual != null) {
+      where.c_fl_manual = Oracle.encodeBool(params.isManual)
+    }
+
+    def result = hid.getTableData(getRefsTable(), where: where)
+    if (result) {
+      result.each { ref ->
+        if (!REFS_CACHE.containsKey(ref.vc_code)) {
+          REFS_CACHE[ref.vc_code] = Numeric.toIntSafe(ref.n_ref_id)
+        }
+      }
+    }
+    return result
+  }
+
+  LinkedHashMap getRefBy(LinkedHashMap input) {
+    return getRefsBy(input)?.getAt(0)
+  }
+
+  LinkedHashMap getRefByCode(def code) {
+    return getRefBy(code: code)
+  }
+
+  LinkedHashMap getRefByName(def code) {
+    return getRefBy(name: name)
+  }
+
   def getRefIdByCode(def code) {
     if (REFS_CACHE.containsKey(code)) {
       return REFS_CACHE[code]
-    } else {
-      def refId = Numeric.toIntSafe(hid.queryFirst("""
-      SELECT SI_REF_PKG_S.GET_ID_BY_CODE('${code.toString()}') FROM DUAL
-      """)?.getAt(0))
-
-      if (refId) {
-        REFS_CACHE[code] = refId
-      }
-      return refId
     }
+
+    def where = [
+      vc_code: code
+    ]
+    def id = Numeric.toIntSafe(hid.getTableData(getRefsTable(), 'n_ref_id', where))
+    if (id) {
+      REFS_CACHE[code] = id
+    }
+    return id
   }
 
-  String getRefCodeById(def id) {
+  def getRefIdByName(String name) {
+    return Numeric.toIntSafe(getRefBy(name: name)?.n_ref_id)
+  }
+
+  String getRefCode(def id) {
     id = Numeric.toIntSafe(id)
     if (REFS_CACHE.containsValue(id)) {
       return REFS_CACHE.find{it.value == id}?.key
-    } else {
-      String refCode = hid.queryFirst("""
-      SELECT SI_REF_PKG_S.GET_CODE_BY_ID(${id}) FROM DUAL
-      """)?.getAt(0)
-
-      if (refCode) {
-        REFS_CACHE[refCode] = id
-      }
-      return refCode
     }
+
+    def where = [
+      n_ref_id: id
+    ]
+    def code = hid.getTableData(getRefsTable(), 'vc_code', where)
+    if (code) {
+      REFS_CACHE[code] = id
+    }
+    return code
+  }
+
+  String getRefCodeById(def id) {
+    return getRefCode(id)
+  }
+
+  String getRefName(def id) {
+    return getRef(id)?.vc_name
   }
 
   String getRefNameById(def id) {
-    String refName = hid.queryFirst("""
-      SELECT SI_REF_PKG_S.GET_NAME_BY_ID(${id}) FROM DUAL
-    """)?.getAt(0)
-    return refName
+    return getRefName(id)
   }
 
   def getDefaultCurrency() {
