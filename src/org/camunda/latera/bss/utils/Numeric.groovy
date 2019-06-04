f9bca9642208ed88d3d09f4083a3d87a149e7b98
dolfinus <m.martynov@latera.ru>
2019-06-04 19:16:35 +0300

    Add isInteger, isFloat, isNumber to Numeric class

--- src/org/camunda/latera/bss/utils/Numeric.groovy
+++ src/org/camunda/latera/bss/utils/Numeric.groovy
@@ -1,5 +1,7 @@
 package org.camunda.latera.bss.utils
 
+import org.camunda.latera.bss.utils.StringUtil
+
 class Numeric {
   static def toIntSafe(def value, def defaultValue = null) {
     if (!value.respondsTo("toBigInteger")) {
@@ -16,10 +18,29 @@ class Numeric {
     if (!value.respondsTo("toBigDecimal")) {
       return defaultValue
     } else try {
+      if (StringUtil.isString(value)) {
+        value = value.replace(',', '.')
+      }
       return value.toBigDecimal()
     }
     catch (NumberFormatException e) {
       return defaultValue
     }
+  }
+
+  static def isInteger(def value) {
+    return toIntSafe(value, null) != null
+  }
+
+  static def isInt(def value) {
+    return isInteger(value)
+  }
+
+  static def isFloat(def value) {
+    return toFloatSafe(value, null) != null
+  }
+
+  static def isNumber(def value) {
+    return isInteger(value) || isFloat(value)
   }
 }
