42b0665988c3db01fc8be30900a590e2db0aeb5c
dolfinus <martinov_m_s_@mail.ru>
2019-07-14 12:42:32 +0300

    Fix STARTTLS issue with GMail SMTP Use String 'true' instead of boolean true for connection params

--- src/org/camunda/latera/bss/connectors/Mail.groovy
+++ src/org/camunda/latera/bss/connectors/Mail.groovy
@@ -7,10 +7,9 @@ import javax.activation.*
 import javax.mail.Message
 
 class MailSender {
-
-  String     host
-  Integer    port
-  String     user
+  private String     host
+  private Integer    port
+  private String     user
   private String     password
   private Session    session
   private Message    message
@@ -26,17 +25,57 @@ class MailSender {
     this.password = ENV['SMTP_PASSWORD'] ?: execution.getVariable('smtpPassword')
 
     this.props = System.getProperties()
-    this.props.put("mail.smtp.auth",           'true')
-    this.props.put("mail.smtp.starttls.enable", true)
-    this.props.put("mail.smtp.ssl.trust",       true)
-    this.props.put("mail.smtp.host",            host)
-    this.props.put("mail.smtp.port",            port)
-    this.props.put("mail.smtp.user",            user)
-    this.props.put("mail.smtp.password",        password)
+    this.props.put('mail.smtp.auth',            'true')
+    this.props.put('mail.smtp.starttls.enable', 'true')
+    this.props.put('mail.smtp.ssl.trust',       'true')
+    this.props.put('mail.smtp.host',             host)
+    this.props.put('mail.smtp.port',             port)
+    this.props.put('mail.smtp.user',             user)
+    this.props.put('mail.smtp.password',         password)
 
     this.session   = Session.getDefaultInstance(props, null)
     this.message   = new MimeMessage(session)
     this.multipart = new MimeMultipart()
+  }
+
+  MailSender getHost() {
+    return this.host
+  }
+
+  MailSender setHost(CharSequence host) {
+    this.host = host
+    this.props.put("mail.smtp.host", host)
+    return this
+  }
+
+  MailSender getPort() {
+    return this.port
+  }
+
+  MailSender setPort(Integer port) {
+    this.port = port
+    this.props.put("mail.smtp.port", port)
+    return this
+  }
+
+  MailSender setPort(CharSequence port) {
+    return setPort(port.toInteger())
+  }
+
+  MailSender getUser() {
+    return this.user
+  }
+
+  MailSender setUser(CharSequence user) {
+    this.user = user
+    this.props.put("mail.smtp.user", user)
+    return this
+  }
+
+  MailSender setPassword(CharSequence password) {
+    this.password = password
+    this.props.put("mail.smtp.password", password)
+    return this
   }
 
   MailSender setFrom(CharSequence from) {
