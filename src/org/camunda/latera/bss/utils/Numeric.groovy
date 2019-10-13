3449ce63314e29976b32689a366dc77550ae703a
dolfinus <martinov_m_s_@mail.ru>
2019-10-13 22:13:01 +0300

    Add tests and documentation for Numeric class

--- src/org/camunda/latera/bss/utils/Numeric.groovy
+++ src/org/camunda/latera/bss/utils/Numeric.groovy
@@ -4,6 +4,14 @@ import java.math.*
 import static org.camunda.latera.bss.utils.StringUtil.isString
 
 class Numeric {
+  /**
+    Convert input to BigInt or return default value.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23toIntSafe+without+default+value"></iframe>
+    @param value Any object to convert
+    @param defaultValue If not possible to convert, value to return. Default null
+  */
   static def toIntSafe(def value, def defaultValue = null) {
     if (!value.respondsTo("toBigInteger")) {
       return defaultValue
@@ -17,6 +25,14 @@ class Numeric {
     }
   }
 
+  /**
+    Convert input to BigDecimal or return default value.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23toFloatSafe+without+default+value"></iframe>
+    @param value Any object to convert
+    @param defaultValue If not possible to convert, value to return. Default null
+  */
   static def toFloatSafe(def value, def defaultValue = null) {
     if (!value.respondsTo("toBigDecimal")) {
       return defaultValue
@@ -33,31 +49,89 @@ class Numeric {
     }
   }
 
+  /**
+    Check whether input is Integer or not.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23isInteger"></iframe>
+    @param value Any object to check
+  */
   static Boolean isInteger(def value) {
     return toIntSafe(value, null) != null
   }
 
+  /**
+    Check whether input is Float or not.
+    <p>
+    Alias for #isInteger()
+    @param value Any object to check
+    @see #isInteger(def)
+  */
   static Boolean isInt(def value) {
     return isInteger(value)
   }
 
+  /**
+    Check whether input is Float or not.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23isFloat"></iframe>
+    @param value Any object to check
+  */
   static Boolean isFloat(def value) {
     return toFloatSafe(value, null) != null
   }
 
+  /**
+    Check whether input is Number (Integer or Float) or not.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23isNumber"></iframe>
+    @param value Any object to check
+  */
   static Boolean isNumber(def value) {
     return isInteger(value) || isFloat(value)
   }
 
+  /**
+    Round number to specified digits after delimiter.
+    <p>
+    If argument cannot be converted into BigDecimal, 0 returned.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23round"></iframe>
+    @param number Float to round
+    @param digits Digits number. Default 2
+  */
   static BigDecimal round(def number, Integer digits = 2) {
-    return toFloatSafe(number, 0).setScale(digits, BigDecimal.ROUND_HALF_UP)
+    return toFloatSafe(number, new BigDecimal(0)).setScale(digits, BigDecimal.ROUND_HALF_UP)
   }
 
+  /**
+    Return maximal value between two numbers.
+    <p>
+    If argument cannot be converted into BigDecimal, 0 is used instead.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23max"></iframe>
+    @param first First number
+    @param second Second number
+  */
   static BigDecimal max(def first, def second) {
-    return toFloatSafe(first, 0).max(toFloatSafe(second, 0))
+    return toFloatSafe(first, new BigDecimal(0)).max(toFloatSafe(second, new BigDecimal(0)))
   }
 
+  /**
+    Return minimal value between two numbers.
+    <p>
+    If argument cannot be converted into BigDecimal, 0 is used instead.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.NumericSpec.html#%23min"></iframe>
+    @param first First number
+    @param second Second number
+  */
   static BigDecimal min(def first, def second) {
-    return toFloatSafe(first, 0).min(toFloatSafe(second, 0))
+    return toFloatSafe(first, new BigDecimal(0)).min(toFloatSafe(second, new BigDecimal(0)))
   }
 }
---
+++ test/org/camunda/latera/bss/utils/NumericSpec.groovy
@@ -0,0 +1,292 @@
+package org.camunda.latera.bss.utils
+
+import spock.lang.*
+import org.camunda.latera.bss.utils.Numeric
+
+class NumericSpec extends Specification {
+  def "#toIntSafe without default value"() {
+    expect:
+    Numeric.toIntSafe(input) == result
+
+    where:
+    input|result
+    1    |1
+    1.1  |1
+    '1'  |1
+    '1.1'|null
+    '1,1'|null
+    'a'  |null
+    null |null
+    false|null
+    []   |null
+    [:]  |null
+  }
+
+  def "#toIntSafe with default value"() {
+    expect:
+    Numeric.toIntSafe(input, defaultValue) == result
+
+    where:
+    input|defaultValue||result
+    1    |0           ||1
+    1.1  |0           ||1
+    '1'  |0           ||1
+    '1.1'|0           ||0
+    '1,1'|0           ||0
+    'a'  |0           ||0
+    null |0           ||0
+    false|0           ||0
+    []   |0           ||0
+    [:]  |0           ||0
+  }
+
+  def "#toFloatSafe without default value"() {
+    expect:
+    Numeric.toFloatSafe(input) == result
+
+    where:
+    input|result
+    1    |1
+    1.1  |1.1
+    '1'  |1
+    '1.1'|1.1
+    '1,1'|1.1
+    'a'  |null
+    null |null
+    false|null
+    []   |null
+    [:]  |null
+  }
+
+  def "#toFloatSafe with default value"() {
+    expect:
+    Numeric.toFloatSafe(input, defaultValue) == result
+
+    where:
+    input|defaultValue||result
+    1    |0           ||1
+    1.1  |0           ||1.1
+    '1'  |0           ||1
+    '1.1'|0           ||1.1
+    '1,1'|0           ||1.1
+    'a'  |0           ||0
+    null |0           ||0
+    false|0           ||0
+    []   |0           ||0
+    [:]  |0           ||0
+  }
+  def "#isInteger"() {
+    expect:
+    Numeric.isInteger(input) == result
+
+    where:
+    input|result
+    1    |true
+    1.1  |true
+    '1'  |true
+    '1.1'|false
+    '1,1'|false
+    'a'  |false
+    null |false
+    false|false
+    []   |false
+    [:]  |false
+  }
+
+  def "#isFloat"() {
+    expect:
+    Numeric.isFloat(input) == result
+
+    where:
+    input|result
+    1    |true
+    1.1  |true
+    '1'  |true
+    '1.1'|true
+    '1,1'|true
+    'a'  |false
+    null |false
+    false|false
+    []   |false
+    [:]  |false
+  }
+
+  def "#isNumber"() {
+    expect:
+    Numeric.isNumber(input) == result
+
+    where:
+    input|result
+    1    |true
+    1.1  |true
+    '1'  |true
+    '1.1'|true
+    '1,1'|true
+    'a'  |false
+    null |false
+    false|false
+    []   |false
+    [:]  |false
+  }
+
+  def "#round"() {
+    expect:
+    Numeric.round(input, digits) == result
+
+    where:
+    input  |digits||result
+    1      |0     ||1
+    1      |1     ||1
+    1      |2     ||1
+    1.1    |0     ||1
+    1.1    |1     ||1.1
+    1.1    |2     ||1.1
+    1.11   |0     ||1
+    1.11   |1     ||1.1
+    1.11   |2     ||1.11
+    1.11   |3     ||1.11
+    1.111  |0     ||1
+    1.111  |1     ||1.1
+    1.111  |2     ||1.11
+    1.111  |3     ||1.111
+    1.111  |4     ||1.111
+    '1'    |2     ||1
+    '1.1'  |2     ||1.1
+    '1.11' |2     ||1.11
+    '1.111'|2     ||1.11
+    '1,1'  |2     ||1.1
+    '1,11' |2     ||1.11
+    '1,111'|2     ||1.11
+    'a'    |2     ||0
+    null   |2     ||0
+    false  |2     ||0
+    []     |2     ||0
+    [:]    |2     ||0
+  }
+
+  def "#max"() {
+    expect:
+    Numeric.max(first, second) == result
+
+    where:
+    first    |second ||result
+    1        |-1     ||1
+    1        |0      ||1
+    1        |1      ||1
+    1        |2      ||2
+    1.1      |-1     ||1.1
+    1.1      |0      ||1.1
+    1.1      |1      ||1.1
+    1.1      |1.2    ||1.2
+    1.1      |2      ||2
+    '1'      |0      ||1
+    '1'      |1      ||1
+    '1'      |2      ||2
+    '1'      |'0'    ||1
+    '1'      |'1'    ||1
+    '1'      |'2'    ||2
+    '1.1'    |0      ||1.1
+    '1.1'    |1      ||1.1
+    '1.1'    |1.2    ||1.2
+    '1.1'    |2      ||2
+    '1.1'    |'0'    ||1.1
+    '1.1'    |'1'    ||1.1
+    '1.1'    |'1.2'  ||1.2
+    '1.1'    |'2'    ||2
+    '1,1'    |0      ||1.1
+    '1,1'    |1      ||1.1
+    '1,1'    |1.2    ||1.2
+    '1,1'    |2      ||2
+    '1,1'    |'0'    ||1.1
+    '1,1'    |'1'    ||1.1
+    '1,1'    |'1.2'  ||1.2
+    '1,1'    |'1,2'  ||1.2
+    '1,1'    |'2'    ||2
+    'a'      |2      ||2
+    'a'      |-1     ||0
+    'a'      |0      ||0
+    'a'      |'b'    ||0
+    null     |2      ||2
+    null     |-1     ||0
+    null     |0      ||0
+    null     |null   ||0
+    false    |2      ||2
+    false    |-1     ||0
+    false    |0      ||0
+    false    |true   ||0
+    false    |false  ||0
+    []       |2      ||2
+    []       |-1     ||0
+    []       |0      ||0
+    []       |[]     ||0
+    []       |[1]    ||0
+    [:]      |2      ||2
+    [:]      |-1     ||0
+    [:]      |0      ||0
+    [:]      |[:]    ||0
+    [:]      |[a:1]  ||0
+  }
+
+  def "#min"() {
+    expect:
+    Numeric.min(first, second) == result
+
+    where:
+    first    |second ||result
+    1        |-1     ||-1
+    1        |0      ||0
+    1        |1      ||1
+    1        |2      ||1
+    1.1      |-1     ||-1
+    1.1      |0      ||0
+    1.1      |1      ||1
+    1.1      |1.2    ||1.1
+    1.1      |2      ||1.1
+    '1'      |0      ||0
+    '1'      |1      ||1
+    '1'      |2      ||1
+    '1'      |'0'    ||0
+    '1'      |'1'    ||1
+    '1'      |'2'    ||1
+    '1.1'    |0      ||0
+    '1.1'    |1      ||1
+    '1.1'    |1.2    ||1.1
+    '1.1'    |2      ||1.1
+    '1.1'    |'0'    ||0
+    '1.1'    |'1'    ||1
+    '1.1'    |'1.2'  ||1.1
+    '1.1'    |'2'    ||1.1
+    '1,1'    |0      ||0
+    '1,1'    |1      ||1
+    '1,1'    |1.2    ||1.1
+    '1,1'    |2      ||1.1
+    '1,1'    |'0'    ||0
+    '1,1'    |'1'    ||1
+    '1,1'    |'1.2'  ||1.1
+    '1,1'    |'1,2'  ||1.1
+    '1,1'    |'2'    ||1.1
+    'a'      |2      ||0
+    'a'      |-1     ||-1
+    'a'      |0      ||0
+    'a'      |'b'    ||0
+    null     |2      ||0
+    null     |-1     ||-1
+    null     |0      ||0
+    null     |null   ||0
+    false    |2      ||0
+    false    |-1     ||-1
+    false    |0      ||0
+    false    |true   ||0
+    false    |false  ||0
+    []       |2      ||0
+    []       |-1     ||-1
+    []       |0      ||0
+    []       |[]     ||0
+    []       |[1]    ||0
+    [:]      |2      ||0
+    [:]      |-1     ||-1
+    [:]      |0      ||0
+    [:]      |[:]    ||0
+    [:]      |[a:1]  ||0
+  }
+}
