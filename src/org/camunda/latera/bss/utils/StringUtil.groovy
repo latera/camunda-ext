69a14e199a21b3891e514844704e5c498ca0a1a3
dolfinus <martinov_m_s_@mail.ru>
2019-10-13 19:01:54 +0300

    Add tests and documentation for StringUtil class

--- src/org/camunda/latera/bss/utils/StringUtil.groovy
+++ src/org/camunda/latera/bss/utils/StringUtil.groovy
@@ -3,22 +3,99 @@ import java.nio.charset.Charset
 import java.security.SecureRandom
 
 class StringUtil {
+  /**
+    Check if input is String or GString.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23isString"></iframe>
+    @param input Any object for type check
+  */
   static Boolean isString(def input) {
     return (input instanceof CharSequence)
   }
 
+  /**
+    Check if input is empty String (w/o space and other symbols) or null.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23isEmpty"></iframe>
+    @param input String to check emptiness
+  */
   static Boolean isEmpty(def input) {
     return !notEmpty(input)
   }
 
+  /**
+    Check if input is not empty String (w/o space and other symbols) or null.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23notEmpty"></iframe>
+    @param input String to check emptiness
+  */
+  static Boolean notEmpty(def input) {
+    if (input) {
+      if (isString(input)) {
+        return !!(trim(input) as Boolean)
+      }
+      return !!(input as Boolean)
+    }
+    return false
+  }
+
+  /**
+    Check if input is empty String (w/o space and other symbols) or null.
+    <p>
+    Like #isEmpty() but also determine 'null' or 'NULL' as empty strings. Used in HID class.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23forceIsEmpty"></iframe>
+    @param input String to check emptiness
+    @see #isEmpty(def)
+  */
   static Boolean forceIsEmpty(def input) {
     return !forceNotEmpty(input)
   }
 
+
+  /**
+    Check if input is not empty String (w/o space and other symbols) or null.
+    <p>
+    Like #notEmpty() but also determine 'null' or 'NULL' as empty strings. Used in HID class.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23forceNotEmpty"></iframe>
+    @param input String to check emptiness
+    @see #notEmpty(def)
+  */
+  static Boolean forceNotEmpty(def input) {
+    if (input) {
+      if (isString(input)) {
+        return !!(nvl(input) as Boolean)
+      }
+      return !!(input as Boolean)
+    }
+    return false
+  }
+
+  /**
+    Trim spaces or other indent symbols from begin and end of String. Non-string values returns as String except null
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23trim"></iframe>
+    @param input String for trimming
+    @see #isEmpty(def)
+  */
   static String trim(def input) {
     return input?.toString()?.stripIndent()?.trim()
   }
 
+  /**
+    Convert 'null' and 'NULL' Strings to null value
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23nvl"></iframe>
+    @param input String for null coerce
+  */
   static String nvl(def input) {
     String result = trim(input)
     if (result == 'null' || result == 'NULL') {
@@ -27,34 +104,30 @@ class StringUtil {
     return result
   }
 
+  /**
+    Convert 'null', 'NULL' and empty Strings to null value
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23forceNvl"></iframe>
+    @param input String for null coerce
+    @see #forceIsEmpty(def)
+  */
   static String forceNvl(def input) {
     String result = nvl(input)
-    if (result == '') {
+    if (isEmpty(result)) {
       return null
     }
     return result
   }
 
-  static Boolean notEmpty(def input) {
-    if (input) {
-      if (isString(input)) {
-        return (trim(input) as Boolean)
-      }
-      return (input as Boolean)
-    }
-    return false
-  }
-
-  static Boolean forceNotEmpty(def input) {
-    if (input) {
-      if (isString(input)) {
-        return (nvl(input) as Boolean)
-      }
-      return (input as Boolean)
-    }
-    return false
-  }
-
+  /**
+    Convert UTF-8 string to Oracle charset.
+    Used by HID class.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23unicodeToVarchar"></iframe>
+    @param input String to convert
+  */
   static byte[] unicodeToVarchar(CharSequence input) {
     if (input) {
       return input.getBytes(Charset.forName("ISO-8859-1"))
@@ -62,13 +135,28 @@ class StringUtil {
     return null
   }
 
+  /**
+    Convert Oracle string UTF-8 charset.
+    Used by HID class.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23varcharToUnicode"></iframe>
+    @param input String to convert
+  */
   static String varcharToUnicode(CharSequence input) {
     if (input) {
-      return new String(input.getBytes(Charset.forName("ISO-8859-1")), "UTF-8")
+      return new String((input ?: '').getBytes(Charset.forName("ISO-8859-1")), "UTF-8")
     }
     return null
   }
 
+  /**
+    Convert UTF-8 string to CP-1251 charset.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23unicodeToCP1251"></iframe>
+    @param input String to convert
+  */
   static byte[] unicodeToCP1251(CharSequence input) {
     if (input) {
       return input.getBytes(Charset.forName("CP1251"))
@@ -76,16 +164,31 @@ class StringUtil {
     return null
   }
 
+  /**
+    Convert CP-1251 string UTF-8 charset.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23cp1251ToUnicode"></iframe>
+    @param input String to convert
+  */
   static String cp1251ToUnicode(CharSequence input) {
     if (input) {
-      return new String(input.getBytes(Charset.forName("CP1251")), "UTF-8")
+      return new String((input ?: '').getBytes(Charset.forName("CP1251")), "UTF-8")
     }
     return null
   }
 
+  /**
+    Convert snake_case to camelCase or CamelCase
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23camelize+without+firstUpper"></iframe>
+    @param input String for camelize
+    @param firstUpper If true return first letter capital otherwise not
+  */
   static String camelize(CharSequence input, Boolean firstUpper = false) {
-    List words = input.split(/_/);
-    String first = firstUpper ? capitalize(words[0]) : words[0].toLowerCase();
+    List words = trim(input).split(/_/);
+    String first = firstUpper ? capitalize(words[0]) : decapitalize(words[0]);
     StringBuilder builder = new StringBuilder(first);
     for (int i = 1; i < words.size(); i++) {
       builder.append(capitalize(words[i]))
@@ -93,18 +196,48 @@ class StringUtil {
     return builder.toString()
   }
 
+  /**
+    Alias for #camelize with named args.
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23camelize+with+named+arguments+-+without+firstUpper"></iframe>
+    @param input String for camelize
+    @param firstUpper Whether return first letter capital or not
+    @see #camelize(CharSequence,Boolean)
+  */
   static String camelize(Map options, CharSequence input) {
     return camelize(input, options.firstUpper)
   }
 
+  /**
+    Convert camelCase or CamelCase to snake_case
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23snakeCase"></iframe>
+    @param input String to convert to snake case
+  */
   static String snakeCase(CharSequence input) {
     return input.replaceAll(/([A-Z])/,/_$1/).toLowerCase().replaceAll(/^_/,'').toString()
   }
 
+  /**
+    Convert first word letter to capital one
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23capitalize"></iframe>
+    @param input String to capitalize
+  */
   static String capitalize(CharSequence input){
     return input?.capitalize().toString()
   }
 
+  /**
+    Convert first word letters to lower case
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23decapitalize"></iframe>
+    @param input String to decapitalize
+  */
   static String decapitalize(CharSequence input){
     if (input.size() == 0) {
       return ''
@@ -115,14 +248,37 @@ class StringUtil {
     }
   }
 
+  /**
+    Join list or array items with delimiter
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23join"></iframe>
+    @param input List to join
+    @param delimiter Delimiter, default ''
+  */
   static String join(List input, CharSequence delimiter = ''){
     return input.join(delimiter)
   }
 
+  /**
+    Join list or array non-empty, non-null values or non-'null' string items with delimiter
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23join"></iframe>
+    @param input List to join
+    @param delimiter Delimiter, default ''
+  */
   static String joinNonEmpty(List input, CharSequence delimiter = ''){
     return join(input.findAll{ forceNotEmpty(it) }, delimiter)
   }
 
+  /**
+    Generate random string with specified length
+    <p>
+    Examples:
+    <iframe style="width:100%;height:200px;border:none;" src="/camunda-ext/test-reports/org.camunda.latera.bss.utils.StringUtilSpec.html#%23random"></iframe>
+    @param length String length, default 6
+  */
   static String random(Integer length = 6) {
     SecureRandom random = new SecureRandom()
     String result = new BigInteger(130, random).toString(32)
---
+++ test/org/camunda/latera/bss/utils/StringUtilSpec.groovy
@@ -0,0 +1,358 @@
+package org.camunda.latera.bss.utils
+
+import spock.lang.*
+import java.nio.charset.Charset
+import org.codehaus.groovy.runtime.GStringImpl
+import org.camunda.latera.bss.utils.StringUtil
+
+class StringUtilSpec extends Specification {
+  def "#isString"() {
+    expect:
+    StringUtil.isString(input) == result
+
+    where:
+    input        |result
+    'some string'|true
+    "${true}"    |true
+    null         |false
+    0            |false
+    false        |false
+    []           |false
+    [:]          |false
+  }
+
+  def "#isEmpty"() {
+    expect:
+    StringUtil.isEmpty(input) == result
+
+    where:
+    input        |result
+    'some string'|false
+    ''           |true
+    ' '          |true
+    '\t'         |true
+    '\n'         |true
+    null         |true
+    'null'       |false
+    'NULL'       |false
+    0            |true
+    false        |true
+    []           |true
+    [:]          |true
+  }
+
+  def "#notEmpty"() {
+    expect:
+    StringUtil.notEmpty(input) == result
+
+    where:
+    input        |result
+    'some string'|true
+    ''           |false
+    ' '          |false
+    '\t'         |false
+    '\n'         |false
+    null         |false
+    'null'       |true
+    'NULL'       |true
+    0            |false
+    false        |false
+    []           |false
+    [:]          |false
+  }
+
+  def "#forceIsEmpty"() {
+    expect:
+    StringUtil.forceIsEmpty(input) == result
+
+    where:
+    input        |result
+    'some string'|false
+    ''           |true
+    ' '          |true
+    '\t'         |true
+    '\n'         |true
+    null         |true
+    'null'       |true
+    'NULL'       |true
+    0            |true
+    false        |true
+    []           |true
+    [:]          |true
+  }
+
+  def "#forceNotEmpty"() {
+    expect:
+    StringUtil.forceNotEmpty(input) == result
+
+    where:
+    input        |result
+    'some string'|true
+    ''           |false
+    ' '          |false
+    '\t'         |false
+    '\n'         |false
+    null         |false
+    'null'       |false
+    'NULL'       |false
+    0            |false
+    false        |false
+    []           |false
+    [:]          |false
+  }
+
+  def "#trim"() {
+    expect:
+    StringUtil.trim(input) == result
+
+    where:
+    input            |result
+    '\t   some'      |'some'
+    'some   \t'      |'some'
+    '\t   some   \t' |'some'
+    '\n  some  \n'   |'some'
+    '\nsome\n'       |'some'
+    '\n\t some\n\t ' |'some'
+    's o m e'        |'s o m e'
+    's o\nm e'       |'s o\nm e'
+    null             |null
+    0                |'0'
+    false            |'false'
+    []               |'[]'
+    [:]              |'[:]'
+  }
+
+  def "#nvl"() {
+    expect:
+    StringUtil.nvl(input) == result
+
+    where:
+    input |result
+    'some'|'some'
+    ''    |''
+    null  |null
+    'null'|null
+    'NULL'|null
+    0     |'0'
+    false |'false'
+    []    |'[]'
+    [:]   |'[:]'
+  }
+
+  def "#forceNvl"() {
+    expect:
+    StringUtil.forceNvl(input) == result
+
+    where:
+    input |result
+    'some'|'some'
+    ''    |null
+    null  |null
+    'null'|null
+    'NULL'|null
+    0     |'0'
+    false |'false'
+    []    |'[]'
+    [:]   |'[:]'
+  }
+
+  def "#camelize without firstUpper"() {
+    expect:
+    StringUtil.camelize(input, false) == result
+
+    where:
+    input      |result
+    'some'     |'some'
+    'some_text'|'someText'
+    ''         |''
+  }
+
+  def "#camelize with firstUpper"() {
+    expect:
+    StringUtil.camelize(input, true) == result
+
+    where:
+    input      |result
+    'some'     |'Some'
+    'some_text'|'SomeText'
+    ''         |''
+  }
+
+  def "#camelize with named arguments - without firstUpper"() {
+    expect:
+    StringUtil.camelize(input, firstUpper: false) == result
+
+    where:
+    input      |result
+    'some'     |'some'
+    'some_text'|'someText'
+    ''         |''
+  }
+
+  def "#camelize with named arguments - with firstUpper"() {
+    expect:
+    StringUtil.camelize(input, firstUpper: true) == result
+
+    where:
+    input      |result
+    'some'     |'Some'
+    'some_text'|'SomeText'
+    ''         |''
+  }
+
+  def "#snakeCase"() {
+    expect:
+    StringUtil.snakeCase(input) == result
+
+    where:
+    input      |result
+    'some'     |'some'
+    'someText' |'some_text'
+    'SomeText' |'some_text'
+    ''         |''
+  }
+
+  def "#capitalize"() {
+    expect:
+    StringUtil.capitalize(input) == result
+
+    where:
+    input       |result
+    'some'      |'Some'
+    'Some'      |'Some'
+    'some text' |'Some text'
+    'Some text' |'Some text'
+    'someText'  |'SomeText'
+    'SomeText'  |'SomeText'
+    's'         |'S'
+    'S'         |'S'
+    ''          |''
+  }
+
+  def "#decapitalize"() {
+    expect:
+    StringUtil.decapitalize(input) == result
+
+    where:
+    input       |result
+    'Some'      |'some'
+    'some'      |'some'
+    'Some Text' |'some Text'
+    'some Text' |'some Text'
+    'SomeText'  |'someText'
+    'someText'  |'someText'
+    'S'         |'s'
+    's'         |'s'
+    ''          |''
+  }
+
+  def "#join"() {
+    expect:
+    StringUtil.join(input, delimiter) == result
+
+    where:
+    input                        |delimiter|result
+    [1,2]                        |','      |'1,2'
+    [1,2]                        |' '      |'1 2'
+    [1,2]                        |';'      |'1;2'
+    [1,2,null,'','null',0,false] |','      |'1,2,null,,null,0,false'
+    [1,2,null,'','null',0,false] |' '      |'1 2 null  null 0 false'
+    [1,2,null,'','null',0,false] |';'      |'1;2;null;;null;0;false'
+    []                           |','      |''
+    []                           |';'      |''
+    []                           |' '      |''
+  }
+
+  def "#joinNonEmpty"() {
+    expect:
+    StringUtil.joinNonEmpty(input, delimiter) == result
+
+    where:
+    input                        |delimiter|result
+    [1,2]                        |','      |'1,2'
+    [1,2]                        |' '      |'1 2'
+    [1,2]                        |';'      |'1;2'
+    [1,2,null,'','null',0,false] |','      |'1,2'
+    [1,2,null,'','null',0,false] |' '      |'1 2'
+    [1,2,null,'','null',0,false] |';'      |'1;2'
+    []                           |','      |''
+    []                           |';'      |''
+    []                           |' '      |''
+  }
+
+  def "#random"() {
+    expect:
+    StringUtil.random(inputLen).size() == resultLen
+    StringUtil.random(inputLen) != StringUtil.random(inputLen)
+
+    where:
+    inputLen|resultLen
+    6       |6
+    7       |7
+    8       |8
+  }
+
+  def "#unicodeToVarchar"() {
+    expect:
+    StringUtil.unicodeToVarchar(input) == result as byte[]
+
+    where:
+    input |result
+    'test'|[116, 101, 115, 116]
+    'Test'|[84,  101, 115, 116]
+    '0'   |[48]
+    ' '   |[32]
+    ''    |null
+    null  |null
+  }
+
+  def "#varcharToUnicode"() {
+    expect:
+    if (input != null && input != '') {
+      input = new String(input as byte[], Charset.forName("ISO-8859-1"))
+    }
+    StringUtil.varcharToUnicode(input) == result
+
+    where:
+    input                |result
+    [116, 101, 115, 116] |'test'
+    [84,  101, 115, 116] |'Test'
+    [48]                 |'0'
+    [32]                 |' '
+    ''                   |null
+    []                   |null
+    null                 |null
+  }
+
+  def "#unicodeToCP1251"() {
+    expect:
+    StringUtil.unicodeToCP1251(input) == result as byte[]
+
+    where:
+    input |result
+    'test'|[116, 101, 115, 116]
+    'Test'|[84,  101, 115, 116]
+    '0'   |[48]
+    ' '   |[32]
+    ''    |null
+    null  |null
+  }
+
+  def "#cp1251ToUnicode"() {
+    expect:
+    if (input != null && input != '') {
+      input = new String(input as byte[], Charset.forName("CP1251"))
+    }
+    StringUtil.varcharToUnicode(input) == result
+
+    where:
+    input               |result
+    [116, 101, 115, 116]|'test'
+    [84,  101, 115, 116]|'Test'
+    [48]                |'0'
+    [32]                |' '
+    ''                   |null
+    []                  |null
+    null                |null
+  }
+}
