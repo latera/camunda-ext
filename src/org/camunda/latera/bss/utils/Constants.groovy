d1d17a61321891eb24aa5b0ee8020c3bf73e0cff
dkolesnikov <kolesnikovdmitryi129@gmail.com>
2020-01-09 18:01:07 +0300

    HCX-1 Prettify runCommand,  Add constants, use constants instead of magic numbers in logger, Update docs

---
+++ src/org/camunda/latera/bss/utils/Constants.groovy
@@ -0,0 +1,11 @@
+package org.camunda.latera.bss.utils
+
+class Constants {
+  // Log level
+  static final DEBUG    = 0
+  static final INFO     = 1
+  static final WARNING  = 2
+  static final ERROR    = 3
+  static final CRITICAL = 4
+  static final DEFAULT  = 5
+}
