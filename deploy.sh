fd1e81d67c02ea6012ac3a38abf77fb545650d30
dolfinus <m.martynov@latera.ru>
2020-02-14 13:46:27 +0300

    Allow to build docker images locally with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,17 @@ cd $ROOT_DIR
 cd ./demo_processes && find ./ -type d -maxdepth 1 -mindepth 1 -exec bash -c "cd {} && echo \$(basename \$(pwd))" ';' > $ROOT_DIR/demo_processes/list
 cd $ROOT_DIR
 
+if [[ "x$DOCKERHUB_REPO" != "x" ]]; then
+  export REPO="$DOCKERHUB_REPO"
+else
+  export REPO="latera/camunda"
+fi
+
 export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 export REVISION=$(mvn buildnumber:create help:evaluate -Dexpression=buildNumber -q -DforceStdout)
-docker build --label "org.opencontainers.image.created"="$(date --rfc-3339=seconds)" --label "org.opencontainers.image.revision"="$REVISION" --label "org.opencontainers.image.version"="$VERSION" -t $DOCKERHUB_REPO:$TRAVIS_JOB_ID -f Dockerfile .
+
+if [[ "x$TRAVIS_JOB_ID" != "x" ]]; then
+  docker build --label "org.opencontainers.image.created"="$(date --rfc-3339=seconds)" --label "org.opencontainers.image.revision"="$REVISION" --label "org.opencontainers.image.version"="$VERSION" -t $DOCKERHUB_REPO:$TRAVIS_JOB_ID -f Dockerfile .
+else
+  docker build --label "org.opencontainers.image.created"="$(date --rfc-3339=seconds)" --label "org.opencontainers.image.revision"="$REVISION" --label "org.opencontainers.image.version"="$VERSION" -t $REPO:$VERSION -f Dockerfile .
+fi
