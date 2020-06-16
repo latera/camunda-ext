27015542ae8aca74ee43980fd44d128c85f1cbeb
Maxim Smirnov <m.smirnov@latera.ru>
2020-06-16 17:34:28 +0300

    HOPER-4719 Add processes for  hoper

---
+++ demo_processes/relocate_customer/src/main/resources/scripts/internal/init.groovy
@@ -0,0 +1,11 @@
+import org.camunda.latera.bss.utils.StringUtil
+import org.camunda.latera.bss.connectors.HOMS
+
+def orderCode = execution.getVariable('homsOrderCode')
+
+if (StringUtil.isEmpty(orderCode)) {
+  def homs = new HOMS(execution)
+  homs.createOrder('relocate_customer')
+}
+
+execution.setBusinessKey(execution.getVariable('homsOrderCode'))
